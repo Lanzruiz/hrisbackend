f1f9f0d31a4ee6552619aad2ec0f1a9daf466389
Lanz Ruz <Lanzruizdesigns@gmail.com>
2020-04-06 23:12:24 +0800

    testing post

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const express = require('express');
 
 const authController = require('../controller/login');
@@ -7,6 +6,10 @@ const authController = require('../controller/login');
 
 const router = express();
 
-router.post('/login', authController.getToken);
+//router.post('/login', authController.getToken);
+
+router.post('/login', ()=>{
+    res.send(200).json({sessionToken: 'lsfjlskfjalsaekjdakjsdsjfasds'});
+});
 
 module.exports = router;
