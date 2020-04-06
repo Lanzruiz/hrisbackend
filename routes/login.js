2b180c3b91fe41a885313ef63882fcc1fd5e3158
Lanz Ruz <Lanzruizdesigns@gmail.com>
2020-04-06 23:25:34 +0800

    adding jwt

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const authController = require('../controller/login');
 
@@ -9,7 +11,13 @@ const router = express();
 //router.post('/login', authController.getToken);
 
 router.post('/login', (req, res)=>{
-    res.status(200).json({sessionToken: 'lsfjlskfjalsaekjdakjsdsjfasds'});
+    //res.status(200).json({sessionToken: 'lsfjlskfjalsaekjdakjsdsjfasds'});
+    const user = {
+        user: req.body.username
+    }
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken: accessToken});
 });
 
 module.exports = router;
