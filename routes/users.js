575ddca7e4a565217f49a8ba2922a4e60964c9ae
Lanz Ruz <Lanzruizdesigns@gmail.com>
2020-04-06 22:14:31 +0800

    heruko deploy

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,6 @@ const router = express();
 
 
 router.get('/users/list', loginController.getAuth, usersController.getUsers);
-router.post('/users/create', usersController.createUser);
+router.post('/users/create', loginController.getAuth, usersController.createUser);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const usersRoutes = require('./routes/users');
 const authRoutes = require('./routes/login');
 const bodyParse = require('body-parser');
+const PORT = process.env.PORT || 8000;
 
 
 const app = express();
@@ -11,4 +12,4 @@ app.use(bodyParse.urlencoded());
 
 app.use(usersRoutes);
 app.use(authRoutes);
-app.listen(8000);
+app.listen(PORT);
